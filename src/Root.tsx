0c5e2b5dca17496bb7fa9585c9a197406f36a2bb
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2021-04-18 00:24:46 -0500

    fixed entry naming

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider, ScrollProvider, ThemeProvider } from "context";
-import Entry from "Entry";
+import Entry from "entry";
 // Required for react-native-root-toast for react-native >= 0.62
 import { RootSiblingParent } from "react-native-root-siblings";
 import { MenuProvider } from "react-native-popup-menu";
