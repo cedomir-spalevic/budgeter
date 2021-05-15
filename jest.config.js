bdc1751a2211ae0f9c7bb16bcd526887fdf846bf
Cedomir Spalevic <cedomir.spalevic@gmail.com>
2021-05-14 22:02:24 -0500

    adding basic jest setup

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { defaults: tsjPreset } = require("ts-jest/presets");
+
+module.exports = {
+  preset: "react-native",
+  transform: {
+    ...tsjPreset.transform,
+    "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
+  },
+  globals: {
+    "ts-jest": {
+      babelConfig: true,
+    }
+  },
+  // This is the only part which you can keep
+  // from the above linked tutorial"s config:
+  cacheDirectory: ".jest/cache",
+};
